da2eca4b93fea1c55430ddba2aef6a314330bf46
Maik Silva <maikeduardo@gmail.com>
2024-09-14 19:39:58 -0300

    Implementado Desafio de Código

---
+++ src/pages/cadastro/styles.js
@@ -0,0 +1,108 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+    width: 100%;
+    max-width: 80%;
+    margin: 0 auto;
+    margin-top: 120px;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: top;
+`
+
+export const Wrapper = styled.div`
+    max-width: 300px;
+`
+
+export const Column = styled.div`
+    flex: 1;
+    margin-top: 0;
+`
+
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+
+    margin-top: 20px;
+`
+
+
+
+export const Title = styled.h2`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 32px;
+    max-width: 90%;
+    margin-bottom: 20px;
+    line-height: 44px;
+
+    color: #FFFFFF;
+`
+
+export const TitleLogin = styled.p`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 32px;
+    line-height: 43.58px;
+    margin-bottom: 8px;
+`
+
+export const SubtitleLogin = styled.p`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 25px;
+    margin-bottom: 35px;
+`
+
+
+export const EsqueciText  = styled.p`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 19px;
+
+    color: #E5E044;
+` 
+
+export const CriarText =  styled.p`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 19px;
+    margin-top: 20px;
+    cursor: pointer;
+    color: #23DD7A;
+`
+
+export const NormalText = styled.p`
+    width: 372px;
+    height: 75px;
+    top: 498px;
+    left: 750px;
+    gap: 0px;
+    opacity: 0px;
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    color: #FFFFFF;
+    
+`
+
+export const TenhoConta = styled.p`
+    color: white;
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 19px;
+    margin-top: 20px;
+`
